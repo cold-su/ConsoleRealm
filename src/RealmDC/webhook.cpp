933ced0d65766a64f2656801989c6c63ac0670ae
awaわ <a970565@163.com>
2024-07-05 12:11:23 +0800

    add webhook file

---
+++ src/RealmDC/webhook.cpp
@@ -0,0 +1,1 @@
+﻿#include "webhook.h"
--- src/RealmQQ/LinkDC.cpp
+++ src/RealmQQ/LinkDC.cpp
@@ -8,18 +8,16 @@ namespace Realm::QQ {
 
 	}
 	void LinkDC::Input(nlohmann::json obj){
-
 		std::cout << "QQ函数回调" << std::endl;
+
 
 	}
 
 	void LinkDC::output(void(*Send)(nlohmann::json obj)){
 		RealmQQ::GetRealmBot()->onEvent<twobot::Event::GroupMsg>([Send](const twobot::Event::GroupMsg& msg) {
-
 			//debug
-			std::cout << msg.sender << std::endl;
-
-			(*Send)(msg.sender);
+			std::cout << msg.raw_msg << std::endl;
+			(*Send)(msg.raw_msg);
 			});
 
 	}
