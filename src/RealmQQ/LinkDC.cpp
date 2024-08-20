e9f414ca0751b0c54cc9a174708ff256e0ab3dfd
Coldsu <coldsu@outlook.com>
2024-08-20 18:15:08 +0800

    sth

--- src/RealmQQ/LinkDC.cpp
+++ src/RealmQQ/LinkDC.cpp
@@ -1,41 +1,42 @@
 ﻿#include "LinkDC.h"
 #include "RealmQQ.h"
 
-#include "RealmHashQQ.h"
 #include <iostream>
 #include <sstream>
 #include <string>
+#include "RealmHashQQ.h"
 
 namespace Realm::QQ {
-	void LinkDC::InitLink() {
-		DC::LinkQQ Obj;
-		Obj.OutputMsg(InputMsg);
-	}
-	void LinkDC::InputMsg(nlohmann::json obj) {
-		std::cout << "QQ函数回调" << std::endl;
-
-		std::string str = std::string(obj["channel_id"]);
-		std::stringstream ss(str);
-		dpp::snowflake tmp;
-		ss >> tmp;
-
-		RealmQQ::GetRealmBot()->getApiSet().
-			sendGroupMsg(RealmHashQQ::GetGroup(tmp), ((std::string)obj["usernamecustom"] + ":" + (std::string)obj["content"]));
-	}
-
-	void LinkDC::OutputMsg(void(*Send)(nlohmann::json obj)) {
-		RealmQQ::GetRealmBot()->onEvent<twobot::Event::GroupMsg>([Send](const twobot::Event::GroupMsg& msg) {
-			//debug
-			std::cout << msg.raw_msg << std::endl;
-
-			(*Send)(msg.raw_msg);
-			});
-
-	}
-	void LinkDC::InputRecall(nlohmann::json obj) {
-
-	}
-	void LinkDC::OutRecall(void(*Send)(nlohmann::json obj)) {
-
-	}
+void LinkDC::InitLink() {
+    DC::LinkQQ Obj;
+    Obj.OutputMsg(InputMsg);
 }
+void LinkDC::InputMsg(nlohmann::json obj) {
+    std::cout << "QQ函数回调" << std::endl;
+
+    std::string str = std::string(obj["channel_id"]);
+    std::stringstream ss(str);
+    dpp::snowflake tmp;
+    ss >> tmp;
+
+    uint64_t target_group = RealmHashQQ::GetGroup(tmp);
+    const std::string& message = ((std::string)obj["usernamecustom"] + ":" + (std::string)obj["content"]);
+
+    RealmQQ::GetRealmBot()->getApiSet().sendGroupMsg(target_group, message);
+}
+
+void LinkDC::OutputMsg(void (*Send)(nlohmann::json obj)) {
+    RealmQQ::GetRealmBot()->onEvent<twobot::Event::GroupMsg>([Send](const twobot::Event::GroupMsg& msg) {
+
+#ifdef DEBUG  // debug
+        std::cout << msg.raw_msg << std::endl;
+#endif
+
+        (*Send)(msg.raw_msg);
+    });
+}
+void LinkDC::InputRecall(nlohmann::json obj) {
+}
+void LinkDC::OutRecall(void (*Send)(nlohmann::json obj)) {
+}
+}  // namespace Realm::QQ
